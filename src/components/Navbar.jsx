814f9f92f710d309a241939ed4b8d6e8bcc7e2d6
Kobey Arai <Kobey.Arai@hawaiianair.com>
2025-12-12 22:16:26 -1000

    Minor header improvements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,18 +8,17 @@ import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
+import HomeIcon from "@mui/icons-material/Home";
+import PersonIcon from "@mui/icons-material/Person";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import LogoutIcon from "@mui/icons-material/Logout";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import { Alert } from "react-bootstrap";
 
-const pages = ["edit-profile"];
-const settings = ["edit-profile"];
-
 function Navbar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = useState(null);
   const [error, setError] = useState("");
   const { currentUser, logout, isUserAdmin, adminCheckLoading } = useAuth();
   const navigate = useNavigate();
@@ -27,16 +26,9 @@ function Navbar() {
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
   };
 
   async function handleLogout() {
@@ -54,6 +46,7 @@ function Navbar() {
       <AppBar position="static" style={{ marginBottom: "35px" }}>
         <Container maxWidth="xl">
           <Toolbar disableGutters>
+            {/* Desktop Logo */}
             <Typography
               variant="h6"
               noWrap
@@ -71,74 +64,7 @@ function Navbar() {
               D50 Hawaii Lions CRN Portal
             </Typography>
 
-            {currentUser && (
-              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-                <IconButton
-                  size="large"
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleOpenNavMenu}
-                  color="inherit"
-                >
-                  <MenuIcon />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorElNav}
-                  anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "left",
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "left",
-                  }}
-                  open={Boolean(anchorElNav)}
-                  onClose={handleCloseNavMenu}
-                  sx={{
-                    display: { xs: "block", md: "none" },
-                  }}
-                >
-                  {pages.map((page) => (
-                    <MenuItem key={page} onClick={handleCloseNavMenu}>
-                      <Typography textAlign="center">
-                        <Link
-                          to={`/${page}`}
-                          style={{ textDecoration: "none", color: "black" }}
-                        >
-                          {page}
-                        </Link>
-                      </Typography>
-                    </MenuItem>
-                  ))}
-                  {!adminCheckLoading && isUserAdmin && (
-                    <MenuItem key="admin" onClick={handleCloseUserMenu}>
-                      <Typography textAlign="center">
-                        <Link
-                          to={"admin"}
-                          style={{ textDecoration: "none", color: "black" }}
-                        >
-                          admin
-                        </Link>
-                      </Typography>
-                    </MenuItem>
-                  )}
-                  <MenuItem key="logout" onClick={handleCloseUserMenu}>
-                    <Typography textAlign="center">
-                      <Button
-                        onClick={handleLogout}
-                        style={{ textDecoration: "none", color: "black" }}
-                      >
-                        Log out
-                      </Button>
-                    </Typography>
-                  </MenuItem>
-                </Menu>
-              </Box>
-            )}
-            {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> */}
+            {/* Mobile Logo */}
             <Typography
               variant="h5"
               noWrap
@@ -156,66 +82,61 @@ function Navbar() {
             >
               D50 CRN Portal
             </Typography>
+
+            {/* Desktop Navigation Buttons */}
             {currentUser && (
-              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-                {pages.map((page) => (
-                  <Button
-                    key={page}
-                    onClick={handleCloseNavMenu}
-                    sx={{ my: 2, color: "white", display: "block" }}
-                  >
-                    <Link
-                      to={`/${page}`}
-                      style={{ textDecoration: "none", color: "white" }}
-                    >
-                      {page}
-                    </Link>
-                  </Button>
-                ))}
+              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" }, ml: 3, gap: 2 }}>
+                <Button
+                  component={Link}
+                  to="/"
+                  sx={{ my: 2, color: "white", display: "flex", alignItems: "center" }}
+                  startIcon={<HomeIcon />}
+                >
+                  Home
+                </Button>
+                <Button
+                  component={Link}
+                  to="/edit-profile"
+                  sx={{ my: 2, color: "white", display: "flex", alignItems: "center" }}
+                  startIcon={<PersonIcon />}
+                >
+                  Edit Profile
+                </Button>
                 {!adminCheckLoading && isUserAdmin && (
                   <Button
-                    key="admin"
-                    onClick={handleCloseUserMenu}
-                    sx={{ my: 2, color: "white", display: "block" }}
+                    component={Link}
+                    to="/admin"
+                    sx={{ my: 2, color: "white", display: "flex", alignItems: "center" }}
+                    startIcon={<AdminPanelSettingsIcon />}
                   >
-                    <Link
-                      to="/admin"
-                      style={{ textDecoration: "none", color: "white" }}
-                    >
-                      admin
-                    </Link>
+                    Admin
                   </Button>
                 )}
-                <MenuItem key="logout" onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">
-                    <Button
-                      onClick={handleLogout}
-                      style={{ textDecoration: "none", color: "white" }}
-                    >
-                      Log out
-                    </Button>
-                  </Typography>
-                </MenuItem>
+                <Button
+                  onClick={handleLogout}
+                  sx={{ my: 2, color: "white", display: "flex", alignItems: "center" }}
+                  startIcon={<LogoutIcon />}
+                >
+                  Log Out
+                </Button>
               </Box>
             )}
 
+            {/* Hamburger Menu (Mobile and Desktop) */}
             {currentUser && (
-              <Box sx={{ flexGrow: 0 }}>
-                <Tooltip title="Open settings">
-                  <Button
-                    onClick={handleOpenUserMenu}
-                    sx={{ p: 0 }}
-                    style={{ textDecoration: "none", color: "white" }}
-                  >
-                    My Account
-                  </Button>
-                </Tooltip>
+              <Box>
+                <IconButton
+                  size="large"
+                  aria-label="navigation menu"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
                 <Menu
-                  sx={{ mt: "45px" }}
-                  id="menu-appbar"
-                  anchorEl={anchorElUser}
+                  anchorEl={anchorElNav}
                   anchorOrigin={{
-                    vertical: "top",
+                    vertical: "bottom",
                     horizontal: "right",
                   }}
                   keepMounted
@@ -223,37 +144,26 @@ function Navbar() {
                     vertical: "top",
                     horizontal: "right",
                   }}
-                  open={Boolean(anchorElUser)}
-                  onClose={handleCloseUserMenu}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
                 >
-                  {settings.map((setting) => (
-                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                      <Typography textAlign="center">
-                        <Link
-                          to={`/${setting}`}
-                          style={{ textDecoration: "none", color: "black" }}
-                        >
-                          {setting}
-                        </Link>
-                      </Typography>
-                    </MenuItem>
-                  ))}
+                  <MenuItem component={Link} to="/" onClick={handleCloseNavMenu}>
+                    <HomeIcon sx={{ mr: 1 }} />
+                    <Typography>Home</Typography>
+                  </MenuItem>
+                  <MenuItem component={Link} to="/edit-profile" onClick={handleCloseNavMenu}>
+                    <PersonIcon sx={{ mr: 1 }} />
+                    <Typography>Edit Profile</Typography>
+                  </MenuItem>
                   {!adminCheckLoading && isUserAdmin && (
-                    <MenuItem key="admin" onClick={handleCloseUserMenu}>
-                      <Typography textAlign="center">
-                        <Link
-                          to={"admin"}
-                          style={{ textDecoration: "none", color: "black" }}
-                        >
-                          admin
-                        </Link>
-                      </Typography>
+                    <MenuItem component={Link} to="/admin" onClick={handleCloseNavMenu}>
+                      <AdminPanelSettingsIcon sx={{ mr: 1 }} />
+                      <Typography>Admin</Typography>
                     </MenuItem>
                   )}
-                  <MenuItem key="logout" onClick={handleCloseUserMenu}>
-                    <Typography textAlign="center">
-                      <Button onClick={handleLogout}>Log out</Button>
-                    </Typography>
+                  <MenuItem onClick={() => { handleCloseNavMenu(); handleLogout(); }}>
+                    <LogoutIcon sx={{ mr: 1 }} />
+                    <Typography>Log Out</Typography>
                   </MenuItem>
                 </Menu>
               </Box>
@@ -269,4 +179,5 @@ function Navbar() {
     </div>
   );
 }
+
 export default Navbar;
